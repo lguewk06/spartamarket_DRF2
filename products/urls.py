8cacade01e3a4b9a1ca0cb1dbd505483c50b8f43
lguewk06 <lguewk06@gmail.com>
2024-09-10 09:43:05 +0900

    url 충돌로 인한 API 통합

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='list'),
-    path('create/', views.ProductCreateView.as_view(), name='create'),
-    path('<int:pk>/', views.ProductUpdateView.as_view(), name='update'),
-    path('<int:pk>/', views.ProductDeleteView.as_view(), name='delete'),
+    path('', views.ProductListCreateView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductUpdateDeleteView.as_view(), name='update'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,35 +6,25 @@ from .serializers import ProductSerializer
 
 # Create your views here.
 
+# products 생성과 조회
 
-class ProductCreateView(generics.CreateAPIView):
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    pagination_class = None  # 페이지네이션 설정 가능
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAuthenticated()]
+        return [permissions.AllowAny()]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    permission_classes = [permissions.AllowAny]
-    pagination_class = None  # 페이지네이션 설정 가능
-
-
-class ProductUpdateView(generics.UpdateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        product = super().get_object()
-        if product.author != self.request.user:
-            raise PermissionDenied("니꺼 아니야")
-        return product
-
-
-class ProductDeleteView(generics.DestroyAPIView):
+# products 수정과 삭제
+class ProductUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
